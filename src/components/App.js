110677eb236ae993d19057ddf9acb0fb1fd0bbc6
Tomek Matras <tomasz.matras@developerweb.pl>
2020-06-06 10:24:29 +0200

    add handleDataFetch fn

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,11 @@ import "./App.css";
 import ButtonFetchUsers from "./ButtonFetchUsers";
 import UsersList from "./UsersList";
 
-const API = "https://randomuser.me/api/?results=5";
+const API = "https://randomuser.me/api/?results=1";
 
 class App extends Component {
   state = {
-    users: null,
+    users: [],
   };
   handleDataFetch = () => {
     fetch(API)
@@ -21,9 +21,10 @@ class App extends Component {
       })
       .then((res) => res.json())
       .then((data) => {
-        this.setState({
-          users: data.results,
-        });
+        const user = data.results;
+        this.setState((prevState) => ({
+          users: user.concat(prevState.users),
+        }));
       })
       .catch((err) => console.log(err + " Błąd adresu"));
   };
@@ -32,7 +33,7 @@ class App extends Component {
     return (
       <div>
         <ButtonFetchUsers click={this.handleDataFetch} />
-        {users ? <UsersList user={users} /> : users}
+        {users.length > 0 ? <UsersList user={users} /> : users}
       </div>
     );
   }
